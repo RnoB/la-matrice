81986a241f6b21a4ad079304e6f431f89357bb73
rnoB <rno.bstin@gmail.com>
2020-04-10 11:55:36 +0200

    test server connection

--- server/serverWS.py
+++ server/serverWS.py
@@ -3,11 +3,15 @@ import asyncio
 import pathlib
 import ssl
 import websockets
+import socket
 
 homeFolder = "/home/ubuntu/"
 certFolder = "cert/"
 
-
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(('8.8.8.8', 1))  # connect() for UDP doesn't send packets
+serverIP = s.getsockname()[0]
+s.
 async def hello(websocket, path):
     name = await websocket.recv()
     print(f"< {name}")
@@ -21,7 +25,7 @@ ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
 ssl_context.load_cert_chain(homeFolder+certFolder+"cert.pem",keyfile=homeFolder+certFolder+"privkey.pem")
 
 start_server = websockets.serve(
-    hello, "localhost", 6785, ssl=ssl_context
+    hello, serverIP, 6785, ssl=ssl_context
 )
 
 asyncio.get_event_loop().run_until_complete(start_server)
