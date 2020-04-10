dff96277ea42ca0b0ddc48264fd63e18a5c34eef
rnoB <rno.bstin@gmail.com>
2020-04-10 16:07:20 +0200

    test function added

--- server/serverWS.py
+++ server/serverWS.py
@@ -23,7 +23,7 @@ def getLocalIP():
 
 async def register(websocket):
     players.append(websocket)
-    message = await players[-1].recv
+    message = await players[-1].recv()
     print(message)
     await asyncio.wait([player.send("message") for player in players])
 
