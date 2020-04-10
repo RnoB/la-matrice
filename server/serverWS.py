334601ccea141e8d8081d73ae4bc31ef176f381e
rnoB <rno.bstin@gmail.com>
2020-04-10 15:28:36 +0200

    test server connection

--- server/serverWS.py
+++ server/serverWS.py
@@ -8,10 +8,12 @@ import socket
 homeFolder = "/home/ubuntu/"
 certFolder = "cert/"
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.connect(('8.8.8.8', 1))  # connect() for UDP doesn't send packets
-serverIP = s.getsockname()[0]
-s.close()
+def getLocalIP():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(('8.8.8.8', 1))  # connect() for UDP doesn't send packets
+    localIP = s.getsockname()[0]
+    s.close()
+    return localIP
 
 async def hello(websocket, path):
     name = await websocket.recv()
@@ -22,12 +24,21 @@ async def hello(websocket, path):
     await websocket.send(greeting)
     print(f"> {greeting}")
 
-ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-ssl_context.load_cert_chain(homeFolder+certFolder+"cert.pem",keyfile=homeFolder+certFolder+"privkey.pem")
 
-start_server = websockets.serve(
-    hello, serverIP, 6785, ssl=ssl_context
-)
 
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
+
+def main():
+    ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+    ssl_context.load_cert_chain(homeFolder+certFolder+"cert.pem",keyfile=homeFolder+certFolder+"privkey.pem")
+
+    start_server = websockets.serve(
+        hello, getLocalIP(), 6785, ssl=ssl_context
+    )
+
+    asyncio.get_event_loop().run_until_complete(start_server)
+    asyncio.get_event_loop().run_forever()
+
+
+
+if __name__ == "__main__":
+    main()
