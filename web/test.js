c70c99f76536617b0a2817fb13c51860cb0e623d
rnoB <rno.bstin@gmail.com>
2020-04-13 17:25:49 +0200

    test function added

--- web/test.js
+++ web/test.js
@@ -37,7 +37,7 @@ function receiver(msg)
     {
         id = data.id;
         console.log(data.playerIds);
-        for each (player in data.playerIds)
+        for (const player of data.playerIds)
         {
             console.log(player);
             listPlayers.push({"id" : player,
