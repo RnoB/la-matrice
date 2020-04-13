5ae36c4687ca548febf0430ac0a8b1d3c13db894
rnoB <rno.bstin@gmail.com>
2020-04-13 16:34:06 +0200

    test function added

--- server/serverWS.py
+++ server/serverWS.py
@@ -46,6 +46,7 @@ async def register(websocket):
     players.append(websocket)
     playerIds.append(playerId)
     world = json.dumps({"world" : 1, "objects" : 2,"id" : playerId,"playerIds" : 2})
+    print(world)
     await players[-1].send(json.dumps(world))
     return playerId
 
