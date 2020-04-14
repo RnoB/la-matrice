28c947dbda12e66d3f86c19a44f86c113e352a44
rnoB <rno.bstin@gmail.com>
2020-04-14 12:10:55 +0200

    trying to get objects rendered when they appear

--- web/test.js
+++ web/test.js
@@ -189,7 +189,7 @@ function animate() {
 }
 function render() {
     var t = new Date().getTime();
-    if (frame%100==0 and listPLayers)
+    if (frame%100==0 && listPLayers)
     {
     console.log(listPlayers[0].mesh);
     }
