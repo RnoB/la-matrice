1bed3f8c401197f56872274fdf844100bb7d3498
rnoB <rno.bstin@gmail.com>
2020-04-15 16:17:38 +0200

    added stable server to show up on front page

--- web/test.js
+++ web/test.js
@@ -196,7 +196,7 @@ function receiver(msg)
         console.log(data)
         id = data.id;
 
-        for (let j = 0; j < data.playerIds.length; ++k) 
+        for (let j = 0; j < data.playerIds.length; ++j) 
         {
 
 
@@ -237,7 +237,7 @@ function receiver(msg)
                 var controllerMesh = new THREE.Mesh( geometry, material );
                 controllerMesh.scale.set(.01,.1,.1);
                 playerInfo["controller"+k.toString()+"Position"] = new THREE.Vector3();
-                playerinfo["controller"+k.toString()+"Rotation"] = new THREE.Quaternion();
+                playerInfo["controller"+k.toString()+"Rotation"] = new THREE.Quaternion();
                 playerInfo["controller"+k.toString()+"Mesh"] = controllerMesh;
                 scene.add(controllerMesh);
             }
