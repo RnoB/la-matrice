86ec3e5fbd6927e08a32dd6b6aefb0ff2ff9a884
rnoB <rno.bstin@gmail.com>
2020-04-16 17:42:47 +0200

    test adding network code to dictionnary in JS

--- server/serverWS.py
+++ server/serverWS.py
@@ -65,7 +65,7 @@ async def register(websocket):
 
         for player in playersSocket:
             try:
-                dataWorld = struct.pack('<BiiB', networkCode['newPlayer'],0, playerId,controllers)
+                dataWorld = struct.pack('>BiiB', networkCode['newPlayer'],0, playerId,controllers)
                 
                 print(dataWorld);
                 await player.send( dataWorld)
