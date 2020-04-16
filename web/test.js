39d897c3ad6f4d73251571e7a67f7c8612fc31e1
rnoB <rno.bstin@gmail.com>
2020-04-16 15:30:38 +0200

    test adding network code to dictionnary in JS

--- web/test.js
+++ web/test.js
@@ -250,6 +250,7 @@ function receiver(msg)
             id = data.getInt32(1);
 
             var Nplayers = (data.byteLength-5)/5.0;
+            console.log(Nplayers);
             for (let j = 0; j < Nplayers; ++j) 
             {
 
