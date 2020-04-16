519839800ae8e8fbb51ea8a6746baf905176b410
rnoB <rno.bstin@gmail.com>
2020-04-16 10:35:56 +0200

    test adding network code to dictionnary in JS

--- server/serverWS.py
+++ server/serverWS.py
@@ -12,7 +12,7 @@ import struct
 import csv
 
 homeFolder = "/home/ubuntu/"
-networkCodePath = homeFolder+"la-matrice/web/data/networkCode.csv"
+networkCodePath = homeFolder+"la-matrice/web/js/network/networkCode.csv"
 certFolder = "cert/"
 
 networkCode = []
