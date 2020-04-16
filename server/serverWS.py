d62a65a9a7dd9b88fd66d9d1c0b85915cec7279f
rnoB <rno.bstin@gmail.com>
2020-04-16 18:48:42 +0200

    test adding network code to dictionnary in JS

--- server/serverWS.py
+++ server/serverWS.py
@@ -143,11 +143,21 @@ def storePosition(code,idPlayer,message):
 
     message = struct.pack('B', networkCode['objectPosition'])
     message += struct.pack('<i', idPlayer)
-    message += struct.pack('<fff',playersPosition[playerIds.index(idPlayer)]['position'])
-    message += struct.pack('<ffff',playersPosition[playerIds.index(idPlayer)]['rotation'])
+    message += struct.pack('<fff',playersPosition[playerIds.index(idPlayer)]['position'][0],\
+                            '<fff',playersPosition[playerIds.index(idPlayer)]['position'][1],\
+                            '<fff',playersPosition[playerIds.index(idPlayer)]['position'][2])
+    message += struct.pack('<ffff',playersPosition[playerIds.index(idPlayer)]['rotation'][0],\
+                                    playersPosition[playerIds.index(idPlayer)]['rotation'][1],\
+                                    playersPosition[playerIds.index(idPlayer)]['rotation'][2],\
+                                    playersPosition[playerIds.index(idPlayer)]['rotation'][3])
     for k in range(0,playersPosition[playerIds.index(idPlayer)]['controllers']):
-        message += struct.pack('<fff',playersPosition["posC"+str(k)]['position'])
-        message += struct.pack('<ffff',playersPosition["rotC"+str(k)]['rotation'])
+        message += struct.pack('<fff',playersPosition["posC"+str(k)]['position'][0],\
+                                        playersPosition["posC"+str(k)]['position'][1],\
+                                        playersPosition["posC"+str(k)]['position'][2])
+        message += struct.pack('<ffff',playersPosition["rotC"+str(k)]['rotation'][0],\
+                                        playersPosition["rotC"+str(k)]['rotation'][1],\
+                                        playersPosition["rotC"+str(k)]['rotation'][2],\
+                                        playersPosition["rotC"+str(k)]['rotation'][3])
     return message
 
 
