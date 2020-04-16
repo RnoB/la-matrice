44d4f1439d1d15d154b6eb89da39889aee5a66e2
rnoB <rno.bstin@gmail.com>
2020-04-16 10:37:03 +0200

    test adding network code to dictionnary in JS

--- web/js/network/networkCode.js
+++ web/js/network/networkCode.js
@@ -3,9 +3,9 @@
 
 var networkCodePath = "./networkCode.csv";
 
-fetch(networkCodePath)
-  .then(response => response.text())
-  .then(text => console.log(text));
+text = fetch(networkCodePath)
+//  .then(response => response.text())
+//  .then(text => console.log(text));
 
 print(text);
 var elems = {};
