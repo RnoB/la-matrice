9fa785b5c87d64eba26da90b6c9dc48e10e80ebe
rnoB <rno.bstin@gmail.com>
2020-04-16 17:22:53 +0200

    test adding network code to dictionnary in JS

--- web/test.js
+++ web/test.js
@@ -276,9 +276,7 @@ function receiver(msg)
 
             }
         case networkCode['world']:
-            id = data.getInt32(1,true);
-            console.log(id);
-            console.log(data.getInt32(1,true));
+            id = data.getInt32(5,true);
             var Nplayers = (data.byteLength-5)/5.0;
 
             for (let j = 0; j < Nplayers; ++j) 
