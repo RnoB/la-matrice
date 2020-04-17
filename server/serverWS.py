6eab6f96585f0c618896833d084cab084c7fefa8
rnoB <rno.bstin@gmail.com>
2020-04-17 12:56:00 +0200

    test adding network code to dictionnary in JS

--- server/serverWS.py
+++ server/serverWS.py
@@ -186,7 +186,7 @@ async def manager(websocket, path):
             code = message[0]
             
             #print("code : "+str(code))
-            if code == networkCode['playerPosition'] and sendMessage and nextPlayer = playerIds[nextPlayer]:
+            if code == networkCode['playerPosition'] and sendMessage and nextPlayer == playerIds[nextPlayer]:
                 messageSend = storePosition(code,idPlayer,message)
                 t1=time.time()
                 print("id : "+str(idPlayer)+" t :"+str(1/(t1-t0)))
