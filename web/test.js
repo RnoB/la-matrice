0b7b70a2e565325e62aa006d736534b5d3e6fbdc
rnoB <rno.bstin@gmail.com>
2020-04-17 10:28:03 +0200

    test adding network code to dictionnary in JS

--- web/test.js
+++ web/test.js
@@ -404,7 +404,7 @@ async function sender()
         
         ws.send(msgView.buffer);
         var t2 = new Date().getTime();
-        await sleep(500-(t2-t1));
+        await sleep(500);
         t1=t2;
     }
 }
