cd5855a5f31550b6638a648d119e4cbc6d6afba0
rnoB <rno.bstin@gmail.com>
2020-04-17 12:35:55 +0200

    test adding network code to dictionnary in JS

--- server/serverWS.py
+++ server/serverWS.py
@@ -177,7 +177,7 @@ async def manager(websocket, path):
     try:
         async for message in websocket:
             if True :
-                print(dir(websocket))
+                print(websocket.messages)
                 t1=time.time()
                 print("id : "+str(idPlayer)+" t :"+str(1/(t1-t0)))
                 t0=t1
