de0bc9c5c42180b6777e9b66623005c3567eb555
rnoB <rno.bstin@gmail.com>
2020-04-18 16:56:00 +0200

    test function added

--- server/serverWS.py
+++ server/serverWS.py
@@ -56,8 +56,8 @@ def initialPosition():
         print(playersPosition)
         mn = np.mean(playersPosition,axis = 0)
         print(mn)
-        print(np.random.rand(np.shape(mn)))
-        position = mn+np.random.rand(np.shape(mn))
+        print(np.random.rand(1,3))
+        position = mn+np.random.rand(1,3)
         print(position)
         theta = np.arctan2(position[0]-mn[0],position[2]-mn[2])
         
