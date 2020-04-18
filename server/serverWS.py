fb4154de991468d29cb788e65aff2f8e8fd22d16
rnoB <rno.bstin@gmail.com>
2020-04-18 15:42:19 +0200

    test function added

--- server/serverWS.py
+++ server/serverWS.py
@@ -206,7 +206,7 @@ async def manager(websocket, path):
                 if code == networkCode['playerPosition'] and len(websocket.messages) == 0 and idPlayer == playerIds[nextPlayer] and playerNumber>0:
                     messageSend = storePosition(code,idPlayer,message)
                     #t1=time.time()
-                    print("id : "+str(idPlayer)+" t :"+str(1/(t1-t0)))
+                    #print("id : "+str(idPlayer)+" t :"+str(1/(t1-t0)))
                     #t0=t1
                     await send(websocket,messageSend)
                     nextPlayer = ((nextPlayer+1)%playerNumber)
