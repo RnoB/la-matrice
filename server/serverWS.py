c34413bfc68c4af9601fe5991554e83b820e3d2b
rnoB <rno.bstin@gmail.com>
2020-04-18 17:25:00 +0200

    test function added

--- server/serverWS.py
+++ server/serverWS.py
@@ -33,6 +33,28 @@ t0 = time.time()
 #logger.setLevel(logging.INFO)
 #logger.addHandler(logging.StreamHandler())
 
+def euler_to_quaternion(roll, pitch, yaw):
+
+    qx = np.sin(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) - np.cos(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
+    qy = np.cos(roll/2) * np.sin(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.cos(pitch/2) * np.sin(yaw/2)
+    qz = np.cos(roll/2) * np.cos(pitch/2) * np.sin(yaw/2) - np.sin(roll/2) * np.sin(pitch/2) * np.cos(yaw/2)
+    qw = np.cos(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
+
+    return [qx, qy, qz, qw]
+
+def quaternion_to_euler(x, y, z, w):
+
+    t0 = +2.0 * (w * x + y * z)
+    t1 = +1.0 - 2.0 * (x * x + y * y)
+    roll = math.atan2(t0, t1)
+    t2 = +2.0 * (w * y - z * x)
+    t2 = +1.0 if t2 > +1.0 else t2
+    t2 = -1.0 if t2 < -1.0 else t2
+    pitch = math.asin(t2)
+    t3 = +2.0 * (w * z + x * y)
+    t4 = +1.0 - 2.0 * (y * y + z * z)
+    yaw = math.atan2(t3, t4)
+    return [yaw, pitch, roll]
 
 with open(networkCodePath, mode='r') as infile:
     reader = csv.reader(infile)
@@ -59,7 +81,7 @@ def initialPosition():
         position = mn+[dist*np.cos(theta),-mn[1]+1.5,dist*np.cos(theta)]
         theta = np.arctan2(position[2]-mn[2],position[0]-mn[0])
         
-        rotation = np.array([0,theta,0,1])
+        rotation = euler_to_quaternion(0, theta, 0)
 
     return position, rotation
 
