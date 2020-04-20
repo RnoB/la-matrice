c3272c72f96a67ad3ad243fa492a74b5783f8114
rnoB <rno.bstin@gmail.com>
2020-04-20 15:13:18 +0200

    server as a class

--- web/test.js
+++ web/test.js
@@ -77,7 +77,43 @@ function initSky(turbidity = 10,
     sky.scale.setScalar( 450000 );
     scene.add( sky );
 
-    
+    // Add Sun Helper
+    sunSphere = new THREE.Mesh(
+        new THREE.SphereBufferGeometry( 20000, 16, 8 ),
+        new THREE.MeshBasicMaterial( { color: 0xffffff } )
+    );
+    sunSphere.position.y = - 700000;
+    sunSphere.visible = false;
+    scene.add( sunSphere );
+
+    /// GUI
+
+
+
+    var distance = 400000;
+
+    var uniforms = sky.material.uniforms;
+    uniforms[ "turbidity" ].value = turbidity;
+    uniforms[ "rayleigh" ].value = rayleigh;
+    uniforms[ "mieCoefficient" ].value = mieCoefficient;
+    uniforms[ "mieDirectionalG" ].value = mieDirectionalG;
+    uniforms[ "luminance" ].value = luminance;
+    uniforms[ "colorR" ].value = colorR;
+    uniforms[ "colorG" ].value = colorG;
+    uniforms[ "colorB" ].value = colorB;
+
+    var theta = Math.PI * ( inclination - 0.5 );
+    var phi = 2 * Math.PI * ( azimuth - 0.5 );
+
+    sunSphere.position.x = distance * Math.cos( phi );
+    sunSphere.position.y = distance * Math.sin( phi ) * Math.sin( theta );
+    sunSphere.position.z = distance * Math.sin( phi ) * Math.cos( theta );
+
+    sunSphere.visible = sun;
+
+    uniforms[ "sunPosition" ].value.copy( sunSphere.position );
+
+
 
 
 
