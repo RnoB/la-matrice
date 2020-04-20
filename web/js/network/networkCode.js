3cf060270e583c045c048339c2ee2e319060185e
rnoB <rno.bstin@gmail.com>
2020-04-20 12:15:27 +0200

    server as a class

--- web/js/network/networkCode.js
+++ web/js/network/networkCode.js
@@ -39,7 +39,7 @@ function getNetworkCode(path)
     return networkCode
 
 }
-getCodes = function()
+export function getCodes()
 {
 	var networkCode = getNetworkCode("./networkCode.csv");
 	var objectsType = getNetworkCode("./objectType.csv");
