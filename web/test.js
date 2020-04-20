313a2530c1b444faccbf42b08fd93b230e560056
rnoB <rno.bstin@gmail.com>
2020-04-20 15:30:10 +0200

    server as a class

--- web/test.js
+++ web/test.js
@@ -95,10 +95,6 @@ function initSky(turbidity = 10,
 
     var uniforms = sky.material.uniforms;
 
-
-    uniforms[ "mieCoefficient" ].value = mieCoefficient;
-    uniforms[ "mieDirectionalG" ].value = mieDirectionalG;
-    uniforms[ "luminance" ].value = luminance;
     uniforms[ "colorR" ].value = colorR;
     uniforms[ "colorG" ].value = colorG;
     uniforms[ "colorB" ].value = colorB;
