ed1cd7e541e1a3414f757f0d5dce995434989fad
rnoB <rno.bstin@gmail.com>
2020-04-22 16:16:54 +0200

    object adder

--- web/test.js
+++ web/test.js
@@ -57,7 +57,7 @@ var world = 0;
 function setUpWorld()
 {
     console.log("Setting up World")
-    var light = new THREE.DirectionalLight(0xab00ac, 1);
+    var light = new THREE.DirectionalLight(0xab00ac, .1);
     light.position.set(50, 50, 50);
     light.castShadow = true;
     light.shadow.mapSize.width = 512;  // default   
