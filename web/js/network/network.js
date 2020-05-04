96836ce97805d8e754f766295de7bac58eb5a302
rnoB <rno.bstin@gmail.com>
2020-05-04 12:10:48 +0200

    moving objects

--- web/js/network/network.js
+++ web/js/network/network.js
@@ -301,7 +301,7 @@ export class Client
 
     constructor(ip = "matricematrice.xyz",port = 6785,updateFrequency = 20,scene = {},camera = {},controllers = {})
     {
-        console.log(scene);
+
         var self = this;
         var world = -1;
         this.ws = new WebSocket('wss://'+ip+':'+port.toString()); 
@@ -321,6 +321,7 @@ export class Client
 
         this.worldInfo = {};
 
+        this.scene = scene;
     }
 
 }
