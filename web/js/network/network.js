a8eb59815f24f0639dd365b8a5aad6007c4d2863
rnoB <rno.bstin@gmail.com>
2020-05-04 13:07:52 +0200

    moving objects

--- web/js/network/network.js
+++ web/js/network/network.js
@@ -235,7 +235,7 @@ export class Client
 
                 var idx = this.listPlayers.findIndex(x => x.id == readId(data,1));
 
-                consoole.log(idx);
+                console.log(idx);
                 if (idx>-1)
                 {
 
