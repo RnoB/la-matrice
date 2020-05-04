db11bb4388b0b1328a0e8245f1c903e37c85c478
rnoB <rno.bstin@gmail.com>
2020-05-04 15:29:18 +0200

    moving objects

--- web/testClean.js
+++ web/testClean.js
@@ -15,10 +15,10 @@ var sky, floor;
 var scene;
 var camera;
 var renderer = null;
-if (!navigator.xr)
-//{
-//var polyfill = new WebXRPolyfill();
-//}
+if (navigator.xr === 'undefined')
+{
+    var polyfill = new WebXRPolyfill();
+}
 var ws;
 
 
