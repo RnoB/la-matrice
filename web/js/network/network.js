ddb786c54a2076b71fcca2f34ea560b564790020
rnoB <rno.bstin@gmail.com>
2020-05-04 12:23:19 +0200

    moving objects

--- web/js/network/network.js
+++ web/js/network/network.js
@@ -98,6 +98,7 @@ function newObject(data,scene,offset)
 }
 function readWorld(data,scene)
 {
+    console.log("scene : "+scene.toString());
     var worldInfo = {"world" : data.getInt32(1,true),
                             'Nplayers' : data.getInt32(5,true),
                             'Nobjects' : data.getInt32(9,true),
