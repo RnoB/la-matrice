8996ce61cab2339d8c64949f6d80260011be8c26
rnoB <rno.bstin@gmail.com>
2020-05-06 17:15:18 +0200

    moving objects

--- server/serverWS.py
+++ server/serverWS.py
@@ -53,7 +53,7 @@ class Server:
 
             dataWorld = struct.pack('B', networkCode['newObject'])
             dataWorld += struct.pack('<i',objecte['type'])
-            dataWorld += tools.updatePacker(objecte,noRotation = false,self.noControllers,noScale = False)
+            dataWorld += tools.updatePacker(objecte,noRotation = False,noControllers = True,noScale = False)
 
             position0 = objecte['position']
             rotation0 = objecte['rotation']
