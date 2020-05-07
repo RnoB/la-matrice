cd14c46fd375c2caa79e548f84f146801b53ce59
rnoB <rno.bstin@gmail.com>
2020-05-07 17:08:48 +0200

    moving objects

--- web/testSky.js
+++ web/testSky.js
@@ -139,7 +139,7 @@ function initSky() {
     gui.addColor( effectController, "SkyColor2").onChange( guiChanged );
     gui.addColor( effectController, "SkyColor3").onChange( guiChanged );
     gui.addColor( effectController, "SunColor").onChange( guiChanged );
-    gui.add( effectController, "SunIntensity", -1.0, 1.0, 0.001 ).onChange( guiChanged );
+    gui.add( effectController, "SunIntensity", .0, 1.0, 0.001 ).onChange( guiChanged );
     gui.add( effectController, "SunAlpha", 0.0, 5.0, 0.01 ).onChange( guiChanged );
     gui.add( effectController, "SunBeta", 0.0, 5.0, 0.001 ).onChange( guiChanged );
     gui.add( effectController, "distance", 0.0, 10.0, 0.001 ).onChange( guiChanged );
